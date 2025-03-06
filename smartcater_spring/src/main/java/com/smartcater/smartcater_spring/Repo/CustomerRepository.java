1d060ae7fead53f0dc6aeabad386aa1fdd1a5609
Pratapprajapati <115774093+Pratapprajapati@users.noreply.github.com>
2025-03-06 17:15:55 +0530

    Delete smartcater_spring directory

--- smartcater_spring/src/main/java/com/smartcater/smartcater_spring/Repo/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.smartcater.smartcater_spring.Repo;
-
-import com.smartcater.smartcater_spring.Model.Customer;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CustomerRepository extends MongoRepository<Customer, String> {
-}
--- smartcater_spring/src/main/java/com/smartcater/smartcater_spring/Repo/MenuItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.smartcater.smartcater_spring.Repo;
-
-import com.smartcater.smartcater_spring.Model.MenuItem;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MenuItemRepository extends MongoRepository<MenuItem, String> {
-}
